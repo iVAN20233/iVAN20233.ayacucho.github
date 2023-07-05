9fdd04a17a33787c25e6d854d50138f62d4c1f0a
iVAN20233 <138524850+iVAN20233@users.noreply.github.com>
2023-07-05 18:17:56 -0500

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -14,54 +14,23 @@ var map = L.map('map', {
     attribution: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors'
   });
   
-  var wmsLayer = L.tileLayer.wms('http://localhost:8080/geoserver/departamento_ayacucho/wms', {
-    layers: 'departamento_ayacucho:zona arqueologica',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Zona Arqueológica'
-  }).addTo(map);
+  // Definir una función para crear las capas WMS de manera más eficiente
+  function createWMSLayer(layerName, displayName) {
+    return L.tileLayer.wms('http://localhost:8080/geoserver/departamento_ayacucho/wms', {
+      layers: 'departamento_ayacucho:' + layerName,
+      format: 'image/png',
+      transparent: true,
+      attribution: displayName
+    }).addTo(map);
+  }
   
-  var colegiosLayer = L.tileLayer.wms('http://localhost:8080/geoserver/departamento_ayacucho/wms', {
-    layers: 'departamento_ayacucho:colegios_ayacucho',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Colegios en Ayacucho'
-  }).addTo(map);
-  
-  var estadioLayer = L.tileLayer.wms('http://localhost:8080/geoserver/departamento_ayacucho/wms', {
-    layers: 'departamento_ayacucho:estadio_ayacucho',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Estadio Ayacucho'
-  }).addTo(map);
-  
-  var departamentoLayer = L.tileLayer.wms('http://localhost:8080/geoserver/departamento_ayacucho/wms', {
-    layers: 'departamento_ayacucho:departamento_ayacucho',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Departamento Ayacucho'
-  }).addTo(map);
-  
-  var farmaciasLayer = L.tileLayer.wms('http://localhost:8080/geoserver/departamento_ayacucho/wms', {
-    layers: 'departamento_ayacucho:farmacias_ayacucho',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Farmacias Ayacucho'
-  }).addTo(map);
-  
-  var grifoLayer = L.tileLayer.wms('http://localhost:8080/geoserver/departamento_ayacucho/wms', {
-    layers: 'departamento_ayacucho:grifo_ayacucho',
-    format: 'image/png',
-    transparent: true,
-    attribution:'Grifo Ayacucho'
-  }).addTo(map);
-  
-  var hotelesLayer = L.tileLayer.wms('http://localhost:8080/geoserver/departamento_ayacucho/wms', {
-    layers: 'departamento_ayacucho:hoteles_ayacucho',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Hoteles Ayacucho'
-  }).addTo(map);
+  var wmsLayer = createWMSLayer('zona arqueologica', 'Zona Arqueológica');
+  var colegiosLayer = createWMSLayer('colegios_ayacucho', 'Colegios en Ayacucho');
+  var estadioLayer = createWMSLayer('estadio_ayacucho', 'Estadio Ayacucho');
+  var departamentoLayer = createWMSLayer('departamento_ayacucho', 'Departamento Ayacucho');
+  var farmaciasLayer = createWMSLayer('farmacias_ayacucho', 'Farmacias Ayacucho');
+  var grifoLayer = createWMSLayer('grifo_ayacucho', 'Grifo Ayacucho');
+  var hotelesLayer = createWMSLayer('hoteles_ayacucho', 'Hoteles Ayacucho');
   
   basemapGoogleSatellite.addTo(map);
   
@@ -84,4 +53,59 @@ var map = L.map('map', {
     position: 'topright',
     collapsed: false
   }).addTo(map);
+  var map = L.map('map', {
+    center: [-13.940556, -74.719794], // Coordenadas del centro de Ayacucho
+    zoom: 8, // Zoom inicial
+    minZoom: 5, // Zoom mínimo permitido
+    maxZoom: 20 // Zoom máximo permitido
+  });
   
+  var basemapGoogleSatellite = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+  });
+  
+  var basemapOpenStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors'
+  });
+  
+  // Definir una función para crear las capas WMS de manera más eficiente
+  function createWMSLayer(layerName, displayName) {
+    return L.tileLayer.wms('http://localhost:8080/geoserver/departamento_ayacucho/wms', {
+      layers: 'departamento_ayacucho:' + layerName,
+      format: 'image/png',
+      transparent: true,
+      attribution: displayName
+    }).addTo(map);
+  }
+  
+  var wmsLayer = createWMSLayer('zona arqueologica', 'Zona Arqueológica');
+  var colegiosLayer = createWMSLayer('colegios_ayacucho', 'Colegios en Ayacucho');
+  var estadioLayer = createWMSLayer('estadio_ayacucho', 'Estadio Ayacucho');
+  var departamentoLayer = createWMSLayer('departamento_ayacucho', 'Departamento Ayacucho');
+  var farmaciasLayer = createWMSLayer('farmacias_ayacucho', 'Farmacias Ayacucho');
+  var grifoLayer = createWMSLayer('grifo_ayacucho', 'Grifo Ayacucho');
+  var hotelesLayer = createWMSLayer('hoteles_ayacucho', 'Hoteles Ayacucho');
+  
+  basemapGoogleSatellite.addTo(map);
+  
+  var baseMaps = {
+    "Google Satellite": basemapGoogleSatellite,
+    "Google Maps": basemapOpenStreetMap
+  };
+  
+  var overlayMaps = {
+    "Zona Arqueológica": wmsLayer,
+    "Colegios en Ayacucho": colegiosLayer,
+    "Estadio Ayacucho": estadioLayer,
+    "Departamento Ayacucho": departamentoLayer,
+    "Farmacias Ayacucho": farmaciasLayer,
+    "Grifo Ayacucho": grifoLayer,
+    "Hoteles Ayacucho": hotelesLayer
+  };
+  
+  L.control.layers(baseMaps, overlayMaps, {
+    position: 'topright',
+    collapsed: false
+  }).addTo(map);
+    
